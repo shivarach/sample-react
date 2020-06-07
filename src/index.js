e042511a679183cb6443a766a6439c479139329e
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2020-06-07 23:43:10 +0530

    keycloak authentication

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ import App from './App';
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import appointmentsApp from './redux/reducer.js'
+import Keycloak from 'keycloak-js'
 
-const store = createStore(appointmentsApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+let initOptions = {
+    url: 'http://localhost:8081/auth', realm: 'test-realm', clientId: 'test-react-app', onLoad: 'login-required'
+}
 
+let keycloak = new Keycloak(initOptions);
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
+    if (!auth) {
+        window.location.reload();
+    } else {
+        console.info("Authenticated");
+    }
+
+    const store = createStore(appointmentsApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+
+
+}).error(() => {
+    console.error("Authenticated Failed");
+});
